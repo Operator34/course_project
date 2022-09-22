71c5f04d8ca78aeb0e00acece9d9a7b3ce207170
Andrey Loginov <operator34@yandex.ru>
2022-09-22 13:50:46 +0300

    preparation refactoring

---
+++ src/app/App.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import Users from './components/users';
+import SearchStatus from './components/searchStatus';
+import api from "../api/index"
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from "../api"
+import api from "../api/index"
 
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll());
