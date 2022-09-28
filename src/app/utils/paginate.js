1288ff58a89552d00827157eacf7a9e7b040463d
Andrey Loginov <operator34@yandex.ru>
2022-09-28 11:25:32 +0300

    delete console.log

--- src/app/utils/paginate.js
+++ src/app/utils/paginate.js
@@ -1,6 +1,5 @@
 function paginate(items, pageNumber, pageSize) {
     const startIndex = (pageNumber - 1) * pageSize;
-    console.log("startIndex", startIndex);
     return [...items].splice(startIndex, pageSize);
 }
 
