f1975dde93d7691c46033874dc334b0eef8ca23e
Andrey Loginov <operator34@yandex.ru>
2022-10-27 13:45:44 +0300

    Базовая валидация

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "../components/textField";
 
 const Login = () => {
     const [data, setData] = useState({ email: "", password: "" });
+    const [, setErrors] = useState();
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
@@ -10,8 +11,24 @@ const Login = () => {
         }));
         console.log(target.name);
     };
+    useEffect(() => {
+        validate();
+    }, [data]);
+    const validate = () => {
+        const errors = {};
+        for (const fieldName in data) {
+            if (data[fieldName].trim() === "") {
+                console.log(fieldName);
+                errors[fieldName] = `${fieldName} обязательно для заполнения`;
+            }
+        }
+        setErrors(errors);
+        return Object.keys(errors).length === 0 || false;
+    };
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isValid = validate();
+        if (!isValid) return;
         console.log(data);
     };
     return (
