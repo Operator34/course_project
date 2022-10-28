443d9a062a877814fef283013462145093acf512
Andrey Loginov <operator34@yandex.ru>
2022-10-28 13:27:14 +0300

    В валидатор добавлена проверка на наличие цифр и длинну пароля не меннее 8 символов

--- src/app/utils/validador.js
+++ src/app/utils/validador.js
@@ -2,24 +2,35 @@
 export function validador(data, config) {
     const errors = {};
     function validate(validateMethod, data, config) {
+        let statusValidate;
         switch (validateMethod) {
             case "isRequired":
-                if (data.trim() === "") return config.message;
+                statusValidate = data.trim() === "";
                 break;
             case "isEmail": {
                 const emailRexExp = /^\S+@\S+\.\S+$/g;
-                if (!emailRexExp.test(data)) return config.message;
+                statusValidate = !emailRexExp.test(data);
                 break;
             }
             case "isCapitalSymbol": {
                 const capitalRexExp = /[A-Z]+/g;
-                if (!capitalRexExp.test(data)) return config.message;
+                statusValidate = !capitalRexExp.test(data);
+                break;
+            }
+            case "inContainDigit": {
+                const digitRexExp = /\d+/g;
+                statusValidate = !digitRexExp.test(data);
+                break;
+            }
+            case "min": {
+                statusValidate = data.length < config.value;
                 break;
             }
 
             default:
                 break;
         }
+        if (statusValidate) return config.message;
     }
     for (const fieldName in data) {
         for (const validateMethod in config[fieldName]) {
